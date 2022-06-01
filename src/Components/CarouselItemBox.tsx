87d55b9d08556d646c2e238bafc887b189c1c945
crisptells <cochamer@gmx.de>
2022-06-01 17:41:54 +0200

    ImageCarousel

---
+++ src/Components/CarouselItemBox.tsx
@@ -0,0 +1,17 @@
+import { Card } from "@mui/material";
+
+
+function CarouselItemBox(props: any) {
+
+    const {
+        image
+    } = props;
+
+    return(
+        <Card>
+            <img src={image}></img>
+        </Card>
+    )
+}
+
+export default CarouselItemBox
