8a87691bc854147c5c719b393483fb167a1bd0ce
Chrisi43 <christian.reitmeier@outlook.com>
2022-06-08 11:11:54 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,23 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Instrument from './Components/Instrument'
 import { Button, Container, Grid, Table } from '@mui/material'
-import MyButtonTest from './Components/MyButtonTest'
-import MyTableTest from './Components/MyTableTest'
 import NavBar from './Components/NavBar'
-import Tiles from './Components/Tiles'
-import { display } from '@mui/system'
+import Homepage from './Pages/Homepage'
+import { Route, Routes } from 'react-router-dom'
+import Test from './Pages/Test'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Container
-      sx={{
-        padding: "100px"
-      }}
-    >
-      
-      <NavBar></NavBar>
-
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Instrument image="https://lifestyle-decor.de/images/product_images/original_images/Autodekor-Musik-Notenschluessel.png.png"></Instrument>
-      </Grid>
-      <Grid item xs={4}>
-        <Instrument image="https://www.creativefabrica.com/wp-content/uploads/2020/05/13/guitar-in-the-circle-logo-Graphics-4113037-1.jpg"></Instrument>
-      </Grid>
-      <Grid item xs={4}>
-        <Instrument image="https://www.creativefabrica.com/wp-content/uploads/2020/05/13/guitar-in-the-circle-logo-Graphics-4113037-1.jpg"></Instrument>
-      </Grid>
-      <Grid item xs={4}>
-        <Instrument image="https://www.creativefabrica.com/wp-content/uploads/2020/05/13/guitar-in-the-circle-logo-Graphics-4113037-1.jpg"></Instrument>
-      </Grid>
-      
-      <Grid item xs={12} textAlign='center'>
-        <p>Impressum | Kontakt | Help</p>
-      </Grid>
-      
-    </Grid>
-
-    
-
+    <Container>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/test' element={<Test/>}/>
+      </Routes>
     </Container>
   )
-
   
 
 }
---
+++ src/Pages/Test.tsx
@@ -0,0 +1,15 @@
+function Test() {
+
+  return (
+      <>      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p></>
+
+  )
+
+}
+
+export default Test
