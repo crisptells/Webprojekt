dbbc627de59c5b6d8bb33b979a172b193c9d55bd
Chrisi43 <christian.reitmeier@outlook.com>
2022-06-08 11:59:11 +0200

    position fix

--- src/Pages/ProductDetails/ProductDetails.tsx
+++ src/Pages/ProductDetails/ProductDetails.tsx
@@ -1,12 +1,24 @@
+import { Container } from '@mui/material'
+import './ProductDetails.css'
+
+
 function ProductDetails() {
 
     return (
+
+        <Container
+      sx={{
+        padding: "100px"
+      }}
+    >
         <><p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p></>
+
+        </Container>
   
     )
   
--- src/Pages/ProductOverview/ProductOverview.tsx
+++ src/Pages/ProductOverview/ProductOverview.tsx
@@ -1,12 +1,26 @@
+import { Container } from '@mui/material'
+import './ProductOverview.css'
+
+
 function ProductOverview() {
 
     return (
+
+        <Container
+        sx={{
+          padding: "100px"
+        }}
+      >
+
         <><p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p>
         <p>Hello</p></>
+
+
+        </Container>
   
     )
   
