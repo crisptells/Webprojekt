918c58668d1c24f6dff423b6de7d0b3124610d3a
Chrisi43 <christian.reitmeier@outlook.com>
2022-06-29 20:49:44 +0200

    fd

--- src/Components/InstrumentsTable.tsx
+++ src/Components/InstrumentsTable.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 import ListItemComponent from "./ListItemComponent";
 
 function InstumentsTable(props: any){
@@ -6,17 +6,21 @@ function InstumentsTable(props: any){
 
     return(
 
+        <Card>
+
+            <Grid container rowSpacing={5}  className="instruments">
+
+                {instrumentsData?.map((entry : any) => (
+             <Grid item key={entry.title} xs={12} sm={12} md={12} lg={12} className="instrument">
+                <ListItemComponent title={entry.title} description={entry.description} category={entry.category} price={entry.price} pictureLink={entry.pictureLink}/>
+            </Grid>
+                ))}
+
+            </Grid>
+
+        </Card>
+
         
-
-        <Grid container rowSpacing={5}  className="instruments">
-
-            {instrumentsData?.map((entry : any) => (
-                <Grid item key={entry.title} xs={12} sm={12} md={12} lg={12} className="instrument">
-                    <ListItemComponent title={entry.title} description={entry.description} category={entry.category} price={entry.price} pictureLink={entry.pictureLink}/>
-                </Grid>
-            ))}
-
-        </Grid>
     
     )
 }
